7babcfa58dd8f315db272ad6a2fad53bec205cb7
sunnychan <suchan@redhat.com>
2021-01-23 22:04:50 +0800

    update getter and setting

--- rhdm-demo/src/main/java/com/sunny/demo/bean/Customer.java
+++ rhdm-demo/src/main/java/com/sunny/demo/bean/Customer.java
@@ -5,6 +5,30 @@ public class Customer {
 
     private int years;
 
+    public CustomerType getType() {
+        return this.type;
+    }
+
+    public void setType(CustomerType type) {
+        this.type = type;
+    }
+
+    public int getYears() {
+        return this.years;
+    }
+
+    public void setYears(int years) {
+        this.years = years;
+    }
+
+    public int getDiscount() {
+        return this.discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
     private int discount;
 
     // Standard getters and setters
