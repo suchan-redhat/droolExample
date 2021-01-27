846eb8403072ca7c28c96b292f515fdce233c05f
sunnychan <suchan@redhat.com>
2021-01-27 09:22:52 +0800

    fuse

---
+++ fuse-camel-kie-demo/src/main/java/com/sunny/demo/bean/Customer.java
@@ -0,0 +1,34 @@
+package com.sunny.demo.bean;
+
+public class Customer {
+    private int type;
+
+    private int years;
+
+    private int discount;
+
+    public int getType() {
+        return this.type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getYears() {
+        return this.years;
+    }
+
+    public void setYears(int years) {
+        this.years = years;
+    }
+
+    public int getDiscount() {
+        return this.discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+       
+}
